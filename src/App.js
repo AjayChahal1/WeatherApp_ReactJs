944bddbf707f9e484a0495797b55b5f45efc947d
AjayChahal1 <67015970+AjayChahal1@users.noreply.github.com>
2021-10-07 23:41:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import '../node_modules/weathericons/css/weather-icons.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Weather from './app_component/weather.component';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/app_component/form.component.jsx
@@ -0,0 +1,36 @@
+// import React from 'react'
+// import {Form,Button} from 'react-bootstrap';
+
+// const FormComponent = (props) => {
+    
+//     return (
+        
+//         <div className="container">
+//             <Form className="input-form" onSubmit={handleSubmit(onSubmit)}>
+//             <div className="row ">
+//                 <div className="col-md-3 mt-3 offset-md-2">
+//                     <input type="text"
+//                         className="form-control "
+//                         name="city"
+//                         autoComplete="off"
+//                         placeholder="City"
+//                     />
+//                 </div>
+//                 <div className="col-md-3 mt-3">
+//                     <input type="text"
+//                         className="form-control"
+//                         name="country"
+//                         autoComplete="off"
+//                         placeholder="Country"
+//                     />
+//                 </div>
+//                 <div className="col-md-3 mt-3">
+//                     <Button type="submit" className="btn btn-warning col-md-8">Get Weather</Button>
+//                 </div>
+//             </div>
+//         </Form>
+//         </div>
+//     )
+// }
+
+// export default FormComponent
